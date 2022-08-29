afc2a2780aae59adaf3d3e06fd47d4ecbf4754e2
carlos-perez-record <42840434+carlos-perez-record@users.noreply.github.com>
2022-08-29 15:00:03 -0500

    Se hace migracion de la tabla center

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_150327) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_193821) do
+  create_table "centers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "zones", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
