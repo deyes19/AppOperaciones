4ed11567bcb121903a92ea2229e960cd709f0244
deyes19 <deyes1219@gmail.com>
2022-09-02 10:50:16 -0500

    añadir libreria de iconos bootstrap, vista show de zones, mejora en vista index de zones

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -25,7 +25,7 @@ class ZonesController < ApplicationController
 
     respond_to do |format|
       if @zone.save
-        format.html { redirect_to zone_url(@zone), notice: "Zone was successfully created." }
+        format.html { redirect_to zone_url(@zone), notice: "Se ha creado la zona" }
         format.json { render :show, status: :created, location: @zone }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ZonesController < ApplicationController
   def update
     respond_to do |format|
       if @zone.update(zone_params)
-        format.html { redirect_to zone_url(@zone), notice: "Zone was successfully updated." }
+        format.html { redirect_to zone_url(@zone), notice: "La zona fue actualizada" }
         format.json { render :show, status: :ok, location: @zone }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class ZonesController < ApplicationController
     @zone.destroy
 
     respond_to do |format|
-      format.html { redirect_to zones_url, notice: "Zone was successfully destroyed." }
+      format.html { redirect_to zones_url, notice: "La zona fue eliminada" }
       format.json { head :no_content }
     end
   end
