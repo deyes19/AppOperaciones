185ecf31192fb4453126768e6eeedaa43544530d
deyes19 <deyes1219@gmail.com>
2022-12-29 15:51:23 -0500

    mejoras en vistas de importacion de zonas y centros de costo

--- app/services/csv_import_zones_service.rb
+++ app/services/csv_import_zones_service.rb
@@ -1,16 +1,16 @@
 class CsvImportZonesService
-    require 'csv'
-  
-    def call(file)
-      file = File.open(file)
-      csv = CSV.parse(file, headers: true, col_sep: ',')
-      csv.each do |row|
-        z_hash = {}
-        z_hash[:id] = row['IDZONA']
-        z_hash[:description] = row['NOMBRE']
-        Zone.find_or_create_by!(z_hash)
-        # binding.b
-        # p row
-      end
+  require 'csv'
+
+  def call(file)
+    file = File.open(file)
+    csv = CSV.parse(file, headers: true, col_sep: ',')
+    csv.each do |row|
+      z_hash = {}
+      z_hash[:id] = row['IDZONA']
+      z_hash[:description] = row['NOMBRE']
+      Zone.find_or_create_by!(z_hash)
+      # binding.b
+      # p row
     end
+  end
 end
